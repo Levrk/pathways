efed03baa20b9a7732ddedebc67d3217e8a61dea
Levrk <levrolandkalb@gmail.com>
2023-02-23 02:02:54 -0500

    Added Transitions

--- pathways/images/static/js/main.js
+++ pathways/images/static/js/main.js
@@ -1,15 +1,62 @@
+
+window.addEventListener('load', function() {
+    const imageConst = document.getElementById('img');
+    const arrows = document.querySelectorAll(".arrowButton");
+    arrows.forEach(function(arrow) {
+        arrow.classList.add('fadeIn');
+    });
+});
+scrollbars
 function upArrow(url) {
-    window.location.href = url;
+    const imageConst = document.getElementById("img");
+    const arrows = document.querySelectorAll(".arrowButton");
+    imageConst.classList.add("onUp");
+    arrows.forEach(function(arrow) {
+        arrow.classList.remove('fadeIn');
+        arrow.classList.add('fadeOut');
+    });
+    setTimeout(function() {
+        window.location.href = url;
+}, 700);
 }
 
 function downArrow(url) {
-    window.location.href = url;
+    const imageConst = document.getElementById("img");
+    const arrows = document.querySelectorAll(".arrowButton");
+    imageConst.classList.add("onDown");
+    arrows.forEach(function(arrow) {
+        arrow.classList.remove('fadeIn');
+        arrow.classList.add('fadeOut');
+    });
+    setTimeout(function() {
+        window.location.href = url;
+}, 700);
 }
 
 function leftArrow(url) {
-    window.location.href = url;
+    const imageConst = document.getElementById("img");
+    const arrows = document.querySelectorAll(".arrowButton");
+    imageConst.classList.add("onLeft");
+    arrows.forEach(function(arrow) {
+        arrow.classList.remove('fadeIn');
+        arrow.classList.add('fadeOut');
+    });
+    setTimeout(function() {
+        window.location.href = url;
+}, 700);
 }
 
 function rightArrow(url) {
-    window.location.href = url;
+   
+    const imageConst = document.getElementById("img");
+    const arrows = document.querySelectorAll(".arrowButton");
+    imageConst.classList.add("onRight");
+    arrows.forEach(function(arrow) {
+        arrow.classList.remove('fadeIn');
+        arrow.classList.add('fadeOut');
+    });
+    setTimeout(function() {
+        window.location.href = url;
+}, 700);
+    
 }
