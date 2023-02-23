24bbee519e90199f3f9d8d8251fc9319ac9ff0ca
Levrk <levrolandkalb@gmail.com>
2023-02-22 19:55:37 -0500

    added arrow key navigation
    implemented event listener and js functions for up/down/left/right
    
    also changed the old anchors to buttons which trigger the same functions as the arrow keys

---
+++ pathways/images/static/js/main.js
@@ -0,0 +1,15 @@
+function upArrow(url) {
+    window.location.href = url;
+}
+
+function downArrow(url) {
+    window.location.href = url;
+}
+
+function leftArrow(url) {
+    window.location.href = url;
+}
+
+function rightArrow(url) {
+    window.location.href = url;
+}
